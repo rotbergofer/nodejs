9c409e2bdba803f057e4de2a7e332bab11baedbd
rotbergofer <rotbergofer@gmail.com>
2018-12-20 18:02:02 +0200

    Update demo_sql.js

--- demo_sql.js
+++ demo_sql.js
@@ -9,8 +9,8 @@ var con = mysql.createConnection({
 
 con.connect(function(err) {
   if (err) throw err;
-  con.query ( "SELECT name,address FROM customers", function (err, result,fields) {
+  con.query ( "SELECT name,address FROM customers WHERE address LIKE 'S%'", function (err, result,fields) {
     if (err) throw err;
-    console.log(fields);
+    console.log(result);
     });
 });
