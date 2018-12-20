28a320307aebf64dab82a910fbee8a0d5e84dd61
rotbergofer <rotbergofer@gmail.com>
2018-12-20 19:35:53 +0200

    Update expressWebServer.js

--- expressWebServer.js
+++ expressWebServer.js
@@ -1,7 +1,15 @@
 var express=require('express');
 var app=express();
+app.route('/Node').get(function(req,res)
+{
+    res.send("Tutorial on Node");
+});
+app.route('/Angular').get(function(req,res)
+{
+    res.send("Tutorial on Angular");
+});
 app.get('/',function(req,res)
 {
-res.send('Hello World!');
+    res.send('Welcome to Guru99 Tutorials');
 });
 var server=app.listen(8080,function() {});
